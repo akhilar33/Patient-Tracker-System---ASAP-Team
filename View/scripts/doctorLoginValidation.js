748bc0ecd94f5a34d324bdcff667368f7cc213d5
Akhila Reddy <akhilareddy@Akhilas-MacBook-Pro.local>
2023-10-28 00:41:59 -0400

    login form for docotrs implented.

---
+++ View/scripts/doctorLoginValidation.js
@@ -0,0 +1,9 @@
+document.addEventListener( function () {
+    const loginButton = document.getElementById('submit-btn ');
+    const loginForm = document.getElementById('login-form');
+
+    loginButton.addEventListener('click', function () {
+        // Replace with your login logic
+        alert('Login button clicked. Implement your login logic here.');
+    });
+});
